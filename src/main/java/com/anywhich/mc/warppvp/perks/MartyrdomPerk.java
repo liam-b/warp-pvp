5f55978989cc8f66671abcdbdb40811b287fc9fc
BuildTools <liam@anywhich.com>
2021-01-01 16:36:28 +1100

    Added martyrdom perk

---
+++ src/main/java/com/anywhich/mc/warppvp/perks/MartyrdomPerk.java
@@ -0,0 +1,25 @@
+package com.anywhich.mc.warppvp.perks;
+
+import org.bukkit.entity.Player;
+import org.bukkit.entity.TNTPrimed;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class MartyrdomPerk extends Perk {
+    public static final Perks NAME = Perks.MARTYRDOM;
+    public static final int TNT_FUSE = 30;
+
+    public MartyrdomPerk(PerksManager perksManager) {
+        super(perksManager);
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+        if (perksManager.hasPlayerSelectedPerk(player, NAME)) {
+            TNTPrimed tnt = player.getWorld().spawn(player.getEyeLocation(), TNTPrimed.class);
+            tnt.setFuseTicks(TNT_FUSE);
+            tnt.setSource(player);
+        }
+    }
+}
