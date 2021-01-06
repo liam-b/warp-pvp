0dd743030d73ad81d19bba83e3b49c124b61fa1e
BuildTools <liam@anywhich.com>
2021-01-06 18:26:04 +1100

    Added check requiring all players to have selected a loadout before game can start

--- src/main/java/com/anywhich/mc/warppvp/playerdata/PlayerData.java
+++ src/main/java/com/anywhich/mc/warppvp/playerdata/PlayerData.java
@@ -2,12 +2,14 @@ package com.anywhich.mc.warppvp.playerdata;
 
 import com.anywhich.mc.warppvp.abilities.Abilities;
 import com.anywhich.mc.warppvp.perks.Perks;
+import org.bukkit.entity.Player;
 
 public class PlayerData {
     public Abilities selectedAbility;
     public Perks selectedPerk;
+    public boolean isSpectating = false;
 
     public boolean isEligibleToPlay() {
-        return selectedAbility != null;
+        return selectedAbility != null && selectedPerk != null && selectedPerk != null;
     }
 }
